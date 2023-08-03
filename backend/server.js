e2bbe93a9363d309da6fa04f3b3190dcdb1165ce
Harry <harry515952831@gmail.com>
2023-08-04 01:27:29 +1000

    create ins-clone project

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+
+const PORT = 8000;
+//127.0.0.1nod
+app.listen(8000,()=>{
+    console.log("server is running on http://localhost:8000")
+})
